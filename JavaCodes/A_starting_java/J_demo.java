f783a63a55376f58567915c9fef567835a98e3bc
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2024-05-23 08:17:00 +0530

    push

--- JavaCodes/A_starting_java/J_demo.java
+++ JavaCodes/A_starting_java/J_demo.java
@@ -3,5 +3,7 @@ package JavaCodes.A_starting_java;
 public class J_demo {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("heelo");
+        
     }
 }
